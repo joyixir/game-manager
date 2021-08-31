5b87a334c2da606b50d589326e93e948f7ae95d1
Amir Hakimnejad <amirhossein@joyixir.com>
2021-08-31 11:12:39 +0430

    Add 2.1.0. changes
    - Add force lose ability
    - Refactor force level finishers
    - Add level number to LevelData

--- GameManager/Assets/Joyixir/GameManager/Scripts/Level/BaseLevel.cs
+++ GameManager/Assets/Joyixir/GameManager/Scripts/Level/BaseLevel.cs
@@ -13,15 +13,17 @@ namespace Joyixir.GameManager.Scripts.Level
         protected abstract void SubscribeToLevelRelatedEvents();
         protected abstract void UnSubscribeFromLevelRelatedEvents();
 
-        protected bool forcedToFinish;
-        internal bool ForcedToFinish => forcedToFinish;
-        internal bool ForcedToWin;
+        internal bool ForcedToFinish => _forcedFinishStatus != LevelFinishStatus.None;
+        private LevelFinishStatus _forcedFinishStatus;
+        public LevelFinishStatus ForcedFinishStatus => _forcedFinishStatus;
         protected BaseLevelConfig _levelConfig;
         internal BaseLevelConfig LevelConfig => _levelConfig;
-
+        private int _levelNumber;
+        public int LevelNumber => _levelNumber;
 
         public virtual void InitializeLevel(BaseLevelConfig config)
         {
+            _levelNumber = LevelManager.PlayerLevel;
             _levelConfig = config;
             FillVariables();
             PrepareUIRequirementsabstract();
@@ -58,36 +60,81 @@ namespace Joyixir.GameManager.Scripts.Level
 
         protected internal abstract void PrepareUIRequirementsabstract();
 
-
-        internal void ForceFinishLevel()
+        internal void ForceFinishLevel(LevelFinishStatus forceStatus = LevelFinishStatus.None)
         {
-            forcedToFinish = true;
+            _forcedFinishStatus = forceStatus;
             FinishLevel();
         }
     }
 
+    public enum LevelFinishStatus
+    {
+        None,
+        Win,
+        Lose,
+        Draw,
+        Retry
+    }
+
+
     public class LevelData
     {
         public int EarnedMoney;
-        public bool Forced;
+        public LevelFinishStatus ForcedStatus;
         public float Satisfaction;
         public int Score;
+        public int LevelNumber;
         public bool WinStatus;
 
         public static LevelData GenerateFromLevel(BaseLevel level)
         {
             var levelData = new LevelData();
-            levelData.EarnedMoney = level.CalculateEarnedMoney();
-            levelData.Score = level.CalculateScore();
-            levelData.WinStatus = level.IsWon() || level.ForcedToWin;
-            levelData.Satisfaction = level.ForcedToWin ? 1f : level.CalculateSatisfaction();
-            levelData.Forced = level.ForcedToFinish;
+            levelData.ForcedStatus = level.ForcedFinishStatus;
+            levelData.LevelNumber = level.LevelNumber;
+            if (levelData.ForcedStatus == LevelFinishStatus.Retry)
+            {
+                levelData.EarnedMoney = 0;
+                levelData.Score = 0;
+            }
+            else
+            {
+                levelData.EarnedMoney = level.CalculateEarnedMoney();
+                levelData.Score = level.CalculateScore();
+            }
+            switch (levelData.ForcedStatus)
+            {
+                case LevelFinishStatus.Win:
+                    {
+                        levelData.WinStatus = true;
+                        levelData.Satisfaction = 1f;
+                        break;
+                    }
+                case LevelFinishStatus.Lose:
+                    {
+                        levelData.WinStatus = false;
+                        levelData.Satisfaction = 0f;
+                        break;
+                    }
+                case LevelFinishStatus.Retry:
+                    {
+                        levelData.WinStatus = false;
+                        levelData.Satisfaction = 0f;
+                        break;
+                    }
+                case LevelFinishStatus.None:
+                    {
+                        levelData.WinStatus = level.IsWon();
+                        levelData.Satisfaction = level.CalculateSatisfaction();
+                        break;
+                    }
+            }
+
             return levelData;
         }
 
         public override string ToString()
         {
-            var export = $"EarnedMoney: {EarnedMoney}, Score: {Score}, WinStatus: {WinStatus}, Satisfaction: {Satisfaction}, Forced: {Forced}";
+            var export = $"EarnedMoney: {EarnedMoney}, Score: {Score}, WinStatus: {WinStatus}, Satisfaction: {Satisfaction}, ForcedTo?: {ForcedStatus.ToString()}";
             return export;
         }
     }
