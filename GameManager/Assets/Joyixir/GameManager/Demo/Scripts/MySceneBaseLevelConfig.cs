ece604c6ccec503ab39241921bcfd19d76ac3e4e
Amir Hakimnejad <amirhossein@joyixir.com>
2021-10-27 13:12:28 +0330

    Improve examples

--- GameManager/Assets/Joyixir/GameManager/Demo/Scripts/MySceneBaseLevelConfig.cs
+++ GameManager/Assets/Joyixir/GameManager/Demo/Scripts/MySceneBaseLevelConfig.cs
@@ -6,7 +6,7 @@ namespace Joyixir.GameManager.Demo.Scripts
     [CreateAssetMenu(fileName = "GameManagerSceneBaseDemoLevel", menuName = "Joyixir/GameManager/Demo/GameManagerSceneBaseDemoLevel", order = 0)]
     public class MySceneBaseLevelConfig : BaseLevelConfig
     {
-        public override string SceneName => "LevelScene";
+        public override string SceneName => "SceneBasedLevelScene";
         public GameObject Dummy;
         public GameObject SecondDummy;
     }
---
+++ GameManager/Assets/Joyixir/GameManager/Demo/Scripts/MySecondSceneBaseLevel.cs
@@ -0,0 +1,72 @@
+using Joyixir.GameManager.Scripts.Level;
+using UnityEngine;
+
+namespace Joyixir.GameManager.Demo.Scripts
+{
+    public class MySecondSceneBaseLevel : BaseLevel
+    {
+        private MySecondSceneBaseLevelConfig _config;
+        [SerializeField] private Transform dummyPlacement1;
+        [SerializeField] private Transform dummyPlacement2;
+        public override void InitializeLevel(BaseLevelConfig config) // Before starting the level. (Usually behind start panel)
+        {
+            base.InitializeLevel(config);
+            _config = (MySecondSceneBaseLevelConfig)config;
+            InstantiateSomeLevelThings();
+        }
+
+        private void InstantiateSomeLevelThings()
+        {
+            GameObject.Instantiate(_config.Dummy, dummyPlacement1);
+            GameObject.Instantiate(_config.SecondDummy, dummyPlacement2);
+        }
+
+        protected internal override void StartLevel() // When click on start
+        {
+            base.StartLevel();
+        }
+
+        protected override void FillVariables()
+        {
+            base.FillVariables();
+        }
+
+        protected internal override void FinishLevel()
+        {
+            base.FinishLevel();
+        }
+
+        protected override void SubscribeToLevelRelatedEvents() // For example, listen to what player does
+        {
+        }
+
+        protected override void UnSubscribeFromLevelRelatedEvents()
+        {
+        }
+
+        protected internal override int CalculateEarnedMoney()
+        {
+            return 10; // Your estimate
+        }
+
+        protected internal override float CalculateSatisfaction()
+        {
+            return .5f; // Your estimate
+        }
+
+        protected internal override int CalculateScore()
+        {
+            return 100; // Your estimate
+        }
+
+        protected internal override bool IsWon()
+        {
+            return CalculateSatisfaction() > .6f; // Your rules
+        }
+
+        protected internal override void PrepareUIRequirementsabstract()
+        {
+            // Ui can implement your level UI here
+        }
+    }
+}
